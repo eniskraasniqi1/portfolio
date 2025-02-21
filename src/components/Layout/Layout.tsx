0c57932aa1a528981409e6c5406373c68ad29268
Enis Krasniqi <eniskraasniqi@gmail.com>
2025-02-21 22:58:15 +0100

    add footer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,9 +7,15 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="mx-auto min-h-screen h-full w-full max-w-5xl px-4 sm:px-10 md:px-24 bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20">
+    <div className="flex flex-col mx-auto min-h-screen h-full w-full max-w-5xl px-4 sm:px-10 md:px-24 bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20">
       <Header />
-      <main>{children}</main>
+      <main className="flex-1">{children}</main>
+      <footer className="flex justify-center text-zinc-600 dark:text-zinc-400">
+        <p className="py-4">
+          Coded with <span className="text-red-500">❤</span> {" by "}
+          <strong>Enis Krasniqi</strong>.
+        </p>
+      </footer>
     </div>
   );
 };
